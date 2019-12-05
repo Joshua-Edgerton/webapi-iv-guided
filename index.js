f5982bf9597f43372c02e454d079760e76ac99e8
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-05 12:50:38 -0600

    small fixes

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./api/server.js');
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log('\n*** Server Running on ${port} ***\n');
+  console.log('\n*** Server Running on 5000 ***\n');
 });
